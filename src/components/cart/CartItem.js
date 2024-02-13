5076d988e632554125b62f50c9c1b178bf484591
Elvina <149134113+elvina-s@users.noreply.github.com>
2024-02-13 19:49:18 +0100

    Update CartItem.js

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import { useDispatch } from "react-redux";
 import { removeItemFromCart } from "../../redux/cartSlice";
 import fruitsData from "../../data/fruitsData";
+import { NavLink } from "react-router-dom";
 
 const CartItem = ({ cartItem }) => {
     const items = fruitsData.find(item => item.id === cartItem.itemId)
@@ -10,14 +11,18 @@ const CartItem = ({ cartItem }) => {
     return (
         <div className="cartItemParts">
             <div className="cartItemCard">
-                <img
-                    src={items.src}
-                    alt={items.name}
-                    className="cartItemImg"
-                />
+                <NavLink to={`/catalogue/${items.id}`}>
+                    <img
+                        src={items.src}
+                        alt={items.name}
+                        className="cartItemImg"
+                    />
+                </NavLink>
                 <div className="cartItemDetails">
-                    <p className="cartItemName">{items.name}</p>
-                    <p className="cartItemQuantity">{items.quantity}</p>
+                    <NavLink to={`/catalogue/${items.id}`}>
+                        <p className="cartItemName">{items.name}</p>
+                    </NavLink>
+                    <p className="cartItemQuantity">Quantity: {cartItem.quantity}</p>
                     <p className="cartItemPrice">$ {(cartItem.quantity * items.price).toFixed(2)}</p>
                     <button
                         className="removeAnItemButton"
