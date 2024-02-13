0766289c8051af1ddec5b37801c146ab46776e9b
Elvina <rammnuschka@gmail.com>
2024-02-13 01:27:11 +0100

    new commit

--- src/data/CarouselData.js
+++ src/data/CarouselData.js
@@ -2,7 +2,6 @@ import src0 from "./../Imgs/fruits.jpg";
 import src1 from "./../Imgs/pineapples.jpg";
 import src2 from "./../Imgs/mangos.jpg";
 import src3 from "./../Imgs/pitahayas.jpg";
-import src4 from "./../Imgs/mango.jpg";
 
 const CarouselData = [
     {
@@ -24,12 +23,7 @@ const CarouselData = [
         id: 3,
         img: src3, 
         name: "Pitahayas",
-    },
-    {
-        id: 4,
-        img: src4, 
-        name: "Mango",
-    },
+    }
 ]
 
 export default CarouselData;
