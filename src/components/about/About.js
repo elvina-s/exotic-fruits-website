5abc8d17d343be83f27d574ce64c8d4daa530979
Elvina <149134113+elvina-s@users.noreply.github.com>
2024-02-16 21:32:49 +0100

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,7 @@ import "./styles.css"
 const About = () => {
     return (
         <div className="About">
-            <h1 className="aboutHeader"><strong>EXOTIC FRUITS SHOP</strong></h1>
+            <h1 className="aboutHeader"><strong>ABOUT US</strong></h1>
             <h2 className="aboutSubHeader">We work closely with our suppliers, building an extensive knowledge of these rare and hard-to-obtain fruits.
             We are always looking to discover new and exciting fruits and are regularly updating our range, working hard to source organically.
             <br/>Enjoy our collection of fruits below!
